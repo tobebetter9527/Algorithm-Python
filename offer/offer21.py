0759609f580fb81b18bfb42c6fdc243f4824e996
tobebetter9527 <wiljz@qq.com>
2023-01-30 20:38:29 +0800

    offer21 调整数组顺序使奇数位于偶数前面

---
+++ offer/offer21.py
@@ -0,0 +1,18 @@
+# author: tobebetter9527
+# since: 2023/01/30 20:23
+from typing import List
+
+
+class Solution:
+    def exchange(self, nums: List[int]) -> List[int]:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            while left < right and (nums[left] & 1) == 1:
+                left += 1
+            while left < right and (nums[right] & 1 == 0):
+                right -= 1
+            if left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        return nums
