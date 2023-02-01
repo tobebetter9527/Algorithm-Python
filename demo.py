5f5cea7267a89c7791008b403d5d66e0cba955cf
tobebetter9527 <wiljz@qq.com>
2023-02-01 11:39:43 +0800

    offer 21 对数器

---
+++ demo.py
@@ -0,0 +1,5 @@
+# author: tobebetter9527
+# since: 2023/1/12 23:26
+import this
+
+print.__doc__
--- offer/offer21.py
+++ offer/offer21.py
@@ -1,5 +1,6 @@
 # author: tobebetter9527
 # since: 2023/01/30 20:23
+from random import random
 from typing import List
 
 
@@ -16,3 +17,26 @@ class Solution:
                 left += 1
                 right -= 1
         return nums
+
+    def generate_arr(self, max_len, max_value):
+        length = int(random() * max_len)
+        return [int(random() * max_value) for _ in range(length)]
+
+    def check_arr(self, nums):
+        length, idx = len(nums), 0
+        while idx < length and nums[idx] % 2 == 1:
+            idx += 1
+        while idx < length and nums[idx] % 2 == 0:
+            idx += 1
+        if idx < length:
+            print("algo is wrong", nums)
+
+
+if __name__ == "__main__":
+    test_times, max_value, max_len = 1000, 100, 100
+    for i in range(test_times):
+        solu = Solution()
+        nums = solu.generate_arr(max_len, max_value)
+        solu.exchange(nums)
+        solu.check_arr(nums)
+    print("done")
