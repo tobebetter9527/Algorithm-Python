925be2e53a2243cdbd6bde38fcca1522d18caac9
tobebetter9527 <wiljz@qq.com>
2023-02-03 10:55:22 +0800

    offer 14

--- demo.py
+++ demo.py
@@ -1,10 +1,5 @@
 # author: tobebetter9527
 # since: 2023/1/12 23:26
-import this
-
-lst = [1, 79, 2, 6, 3]
-lst.sort()
-print(lst)
-print(lst[0])
-print(lst[1:-1])
-print(lst[5:])
+import requests
+r = requests.get("https://www.runoob.com/")
+print(r.text)
