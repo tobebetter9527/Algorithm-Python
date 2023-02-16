37849ca1441384f3694a5cffca0f74439b522454
tobebetter9527 <wiljz@qq.com>
2023-02-16 20:21:22 +0800

    spider

--- web/WechatSpider.py
+++ web/WechatSpider.py
@@ -73,10 +73,10 @@ class WechatVoiceSpider:
 
 if __name__ == '__main__':
     urls_dict = {}
-    urls_dict['英文早餐'] = 'https://mp.weixin.qq.com/s/oLMkxwXdWq5DwqM3bsm4ng'
-    urls_dict['美剧绝命律师'] = 'https://mp.weixin.qq.com/s/r3VJa95rk4ssb428-yvRfA'
-    urls_dict['英语新闻'] = 'https://mp.weixin.qq.com/s/8d01zQi4LogDwW1MlMfW5w'
-    urls_dict['从零开始学英语'] = 'https://mp.weixin.qq.com/s/ZphMsOMwINpLD-flTcbnBA'
-    date_time = '20230213'
+    urls_dict['英文早餐'] = 'https://mp.weixin.qq.com/s/Dm-J-cbyneo7F0GvYJuT9w'
+    urls_dict['美剧人生切割术'] = 'https://mp.weixin.qq.com/s/1PFjtXFlm0HkZYf-CFyUBA'
+    urls_dict['英语新闻'] = 'https://mp.weixin.qq.com/s/DR7i4kJ2aQYeG-vBnWMwhg'
+    urls_dict['从零开始学英语'] = 'https://mp.weixin.qq.com/s/xt2q5o1JJSSDLKZ3IWvesg'
+    date_time = '20230216'
     spider = WechatVoiceSpider(urls_dict, date_time)
     spider.run()
