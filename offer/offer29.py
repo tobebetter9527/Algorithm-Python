a7fba04aa3bbb8651508cb1ea6383f7b801174c9
tobebetter9527 <wiljz@qq.com>
2023-02-17 18:03:43 +0800

    offer 29

--- offer/offer29.py
+++ offer/offer29.py
@@ -8,27 +8,27 @@ class Solution:
         if not matrix or not matrix[0]:
             return list()
         ans = []
-        startRow, startCol, endRow, endCol = 0, 0, len(matrix) - 1, len((matrix[0])) - 1
-        while startRow <= endRow and startCol <= endCol:
-            if startRow == endRow:
-                for i in range(startCol, endCol + 1):
-                    ans.append(matrix[startRow][i])
-            elif startCol == endCol:
-                for i in range(startRow, endRow + 1):
-                    ans.append(matrix[i][endCol])
+        start_row, start_col, end_row, end_col = 0, 0, len(matrix) - 1, len((matrix[0])) - 1
+        while start_row <= end_row and start_col <= end_col:
+            if start_row == end_row:
+                for i in range(start_col, end_col + 1):
+                    ans.append(matrix[start_row][i])
+            elif start_col == end_col:
+                for i in range(start_row, end_row + 1):
+                    ans.append(matrix[i][end_col])
             else:
-                for i in range(startCol, endCol):
-                    ans.append(matrix[startRow][i])
-                for i in range(startRow, endRow):
-                    ans.append(matrix[i][endCol])
-                for i in range(endCol, startCol, -1):
-                    ans.append(matrix[endRow][i])
-                for i in range(endRow, startRow, -1):
-                    ans.append(matrix[i][startCol])
-            startRow += 1
-            startCol += 1
-            endRow -= 1
-            endCol -= 1
+                for i in range(start_col, end_col):
+                    ans.append(matrix[start_row][i])
+                for i in range(start_row, end_row):
+                    ans.append(matrix[i][end_col])
+                for i in range(end_col, start_col, -1):
+                    ans.append(matrix[end_row][i])
+                for i in range(end_row, start_row, -1):
+                    ans.append(matrix[i][start_col])
+            start_row += 1
+            start_col += 1
+            end_row -= 1
+            end_col -= 1
         return ans
 
 
