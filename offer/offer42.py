73fcdc974f0a957b6a368dddc45b35dfc284978c
tobebetter9527 <wiljz@qq.com>
2023-03-02 19:19:41 +0800

    offer 42

--- offer/offer42.py
+++ offer/offer42.py
@@ -15,3 +15,13 @@ class Solution:
             if count < 0:
                 count = 0
         return ans
+
+    def maxSubArray2(self, nums: List[int]) -> int:
+        ans, dp = nums[0], nums[0],
+        for num in nums[1:]:
+            if dp < 0:
+                dp = num
+            else:
+                dp = dp + num
+            ans = max(ans, dp)
+        return ans
